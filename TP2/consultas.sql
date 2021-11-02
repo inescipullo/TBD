861628c47b018fc4d33a63bdc99e5647bfcf8e55
Ines Cipullo <inescipullo@gmail.com>
2021-11-02 18:33:16 -0300

    update tp2

--- TP2/consultas.sql
+++ TP2/consultas.sql
@@ -1,9 +1,7 @@
 USE `Inmobiliaria`;
 
--- en las consultas donde piden nombre, devolver nombre y apellido o solo nombre?
-
 -- Consulta a
-SELECT nombre FROM Persona  
+SELECT nombre, apellido FROM Persona  
 WHERE codigo IN (SELECT codigo_propietario FROM PoseeInmueble);
 
 -- Consulta b
@@ -11,12 +9,12 @@ SELECT codigo FROM Inmueble
 WHERE precio >= 600000 AND precio <= 700000;
 
 -- Consulta c
-SELECT nombre FROM Persona 
+SELECT nombre, apellido FROM Persona 
 WHERE codigo IN (SELECT codigo_cliente FROM PrefiereZona
                  WHERE nombre_poblacion = 'Santa Fe' AND nombre_zona = 'Norte');
 
 -- Consulta d
-SELECT nombre FROM Persona
+SELECT nombre, apellido FROM Persona
 WHERE codigo IN (SELECT vendedor FROM Cliente
                  WHERE codigo IN (SELECT codigo_cliente FROM PrefiereZona
                                   WHERE nombre_poblacion = 'Rosario' AND nombre_zona = 'Centro'));
@@ -26,7 +24,7 @@ SELECT nombre_zona, COUNT(codigo), AVG(precio) FROM Inmueble
 WHERE nombre_poblacion = 'Rosario' GROUP BY nombre_zona;
 
 -- Consulta f
-SELECT nombre FROM Persona 
+SELECT nombre, apellido FROM Persona 
 WHERE codigo IN (SELECT codigo_cliente FROM PrefiereZona
                  WHERE nombre_poblacion = 'Santa Fe' 
                  GROUP BY codigo_cliente HAVING
